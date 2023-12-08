2924d24e08fb1d45f1003d91d13f4156bfe935c3
Devex Hub <developmentexpert121@gmail.com>
2023-12-08 13:12:52 +0530

    update vercel voting api

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 const userRoutes = require("./routes/userRoutes")
 const voteRoutes = require("./routes/voteRoutes")
 const allowedOrigins = [
-  "http://127.0.0.1:5173"
+ " https://voting-app-2015d.web.app",
+ 'https://voting-app-2015d.firebaseapp.com',
 ];
 app.use(cors({
     origin: allowedOrigins,
